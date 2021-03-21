8118d637391455f35e56a1679a168dbbe1dc5d91
Yaksh Bariya <yakshbari4@gmail.com>
2021-03-21 18:56:45 +0530

    Add social links

--- components/Footer.component.tsx
+++ components/Footer.component.tsx
@@ -6,6 +6,23 @@ export default function Footer() {
   let year = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
+      <div className={styles.socialLinks}>
+        <a href="https://codepen.io/thunder-coding">
+          <img src="codepen.svg" />
+        </a>
+        <a href="https://github.com/thunder-coding">
+          <img src="github.svg" />
+        </a>
+        <a href="https://twitter.com/CodingThunder">
+          <img src="twitter.svg" />
+        </a>
+        <a href="https://discord.gg/YMhxGjzsJ8">
+          <img src="discord.svg" />
+        </a>
+        <a href="https://linkedin.com/in/codingthunder">
+          <img src="linkedin.svg" />
+        </a>
+      </div>
       <p className={styles.p}>© {year} Yaksh Bariya</p>
     </footer>
   );
