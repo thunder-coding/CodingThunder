09f604ae5cf1817ca5786fd32ea358c74a240f0c
thunder-coding <yakshbari4@gmail.com>
2021-03-28 15:29:36 +0530

    Add introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,27 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className={styles.imageContainer}>
-      <div className={styles.imageRing}>
-        <Image
-          className={styles.image}
-          height="300px"
-          width="300px"
-          src="/myimage.jpg"
-          alt="Yaksh Bariya's photo"
-        />
+    <>
+      <div className={styles.imageContainer}>
+        <div className={styles.imageRing}>
+          <Image
+            className={styles.image}
+            height="300px"
+            width="300px"
+            src="/myimage.jpg"
+            alt="Yaksh Bariya's photo"
+          />
+        </div>
       </div>
-    </div>
+      <div className={styles.introduction}>
+        <h1>
+          Hey There <span className={styles.wavyHand}>👋</span>,
+        </h1>
+        <p>
+          I am Yaksh Bariya, a 14 year old programmer who loves to code and
+          explore the world of programming
+        </p>
+      </div>
+    </>
   );
 }
