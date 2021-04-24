c1d1762059226e50a575216721e5d9cc8835e79c
Yaksh Bariya <yakshbari4@gmail.com>
2021-04-24 12:18:47 +0530

    Do not use relative path for footer images

--- components/Footer.component.tsx
+++ components/Footer.component.tsx
@@ -9,27 +9,27 @@ export default function Footer() {
       <div className={styles.socialLinks}>
         <Link href="/codepen">
           <div className={styles.imgContainer}>
-            <img src="codepen.svg" alt="CodePen Logo" />
+            <img src="/codepen.svg" alt="CodePen Logo" />
           </div>
         </Link>
         <Link href="/github">
           <div className={styles.imgContainer}>
-            <img src="github.svg" alt="GitHub Logo" />
+            <img src="/github.svg" alt="GitHub Logo" />
           </div>
         </Link>
         <Link href="/twitter">
           <div className={styles.imgContainer}>
-            <img src="twitter.svg" alt="Twitter Logo" />
+            <img src="/twitter.svg" alt="Twitter Logo" />
           </div>
         </Link>
         <Link href="/discord">
           <div className={styles.imgContainer}>
-            <img src="discord.svg" alt="Discord Logo" />
+            <img src="/discord.svg" alt="Discord Logo" />
           </div>
         </Link>
         <Link href="/linkedin">
           <div className={styles.imgContainer}>
-            <img src="linkedin.svg" alt="LinkedIn Logo" />
+            <img src="/linkedin.svg" alt="LinkedIn Logo" />
           </div>
         </Link>
       </div>
