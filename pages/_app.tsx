50229f48b736a15fea9fd6c522b6e8ea356ae5af
Yaksh Bariya <yakshbari4@gmail.com>
2021-05-22 11:22:01 +0530

    chore: meta properties moved from _app.tsx to index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Footer from '../components/Footer.component';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="description" content="Yaksh Bariya's website" />
-        <meta
-          name="keywords"
-          content="Yaksh Bariya, CodingThunder, Coding Thunder, Yaksh Programmer"
-        />
-        <meta property="og:image" content="/ogimage.jpg" />
-        <meta property="og:title" content="Yaksh Bariya" />
-        <meta property="og:url" content="https://codingthunder.vercel.app" />
-        <meta
-          property="og:description"
-          content="Personal website of Yaksh Bariya"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <title>Yaksh Bariya</title>
-      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
-
+import Head from 'next/head';
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="description" content="Yaksh Bariya's website" />
+        <meta
+          name="keywords"
+          content="Yaksh Bariya, CodingThunder, Coding Thunder, Yaksh Programmer"
+        />{' '}
+        <meta property="og:image" content="/ogimage.jpg" />
+        <meta property="og:title" content="Yaksh Bariya" />
+        <meta property="og:url" content="https://codingthunder.vercel.app" />
+        <meta
+          property="og:description"
+          content="Personal website of Yaksh Bariya"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <title>Yaksh Bariya</title>
+      </Head>
       <div className={styles.imageContainer}>
         <div className={styles.imageRing}>
           <Image
