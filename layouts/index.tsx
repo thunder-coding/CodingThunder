thunder-coding/CodingThunder

commit 44294055d1986ea811cd950ea812c5386ee433ca

Use `next-mdx-enhanced` instead of `@next/mdx`

layouts/index.tsx
@@ -1,0 +1,39 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Blog.module.css';
+
+interface FrontMatter {
+  title: string;
+  ogImage?: string;
+  description: string;
+  tags: ReadonlyArray<string>;
+}
+
+interface props {
+  children: JSX.Element | JSX.Element[];
+  frontMatter: FrontMatter;
+}
+
+export default function MDXPage({ children, frontMatter }: Props) {
+  return (
+    <>
+      <Head>
+        <title>{frontMatter.title}</title>
+        <meta property="description" content={frontMatter.description} />
+        <meta property="og:description" content={frontMatter.description} />
+        <meta property="og:title" content={frontMatter.title} />
+        {frontMatter.ogImage ? (
+          <meta property="og:image" content={frontMatter.ogImage} />
+        ) : (
+          ''
+        )}
+      </Head>
+      <main>
+        <h1 className={styles.blogHeading}>{frontMatter.title}</h1>
+	<img src={frontMatter.ogImage} width="100%" alt="" />
+	<h3>{frontMatter.description}</h3>
+        {children}
+      </main>
+    </>
+  );
+}

mdx.d.ts
@@ -1,0 +1,1 @@
+/// <reference types="@mdx-js/loader" />

next.config.js
@@ -1,10 +1,17 @@
 const rehypePrism = require('@mapbox/rehype-prism');
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [rehypePrism],
+const withMDX = require('next-mdx-enhanced')({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  fileExtensions: ['mdx'],
+
+  remarkPlugins: [],
+  rehypePlugins: [rehypePrism],
+  usesSrc: false,
+  extendFrontMatter: {
+    process: (mdxContent, frontMatter) => {},
+    phase: 'prebuild|loader|both',
   },
+  reExportDataFetching: false,
 });
 
 module.exports = withMDX({

styles/Blog.module.css
@@ -1,0 +1,4 @@
+.blogHeading {
+  text-align: center;
+  font-size: 56px;
+}

styles/globals.css
@@ -32,17 +32,25 @@ a {
   }
 }
 
+@font-face {
+  font-family: 'Fira Code';
+  src: url('/FiraCode.woff') format('woff');
+  font-weight: 470;
+  font-style: monospace;
+}
+
 /**
- *  * atom-dark theme for `prism.js`
- *   * Based on Atom's `atom-dark` theme: https://github.com/atom/atom-dark-syntax
- *    * @author Joe Gibson (@gibsjose)
- *     */
+ *   atom-dark theme for `prism.js`
+ *    Based on Atom's `atom-dark` theme: https://github.com/atom/atom-dark-syntax
+ *  @author Joe Gibson (@gibsjose)
+ */
 
 code[class*='language-'],
 pre[class*='language-'] {
   color: #c5c8c6;
   text-shadow: 0 1px rgba(0, 0, 0, 0.3);
-  font-family: Inconsolata, Monaco, Consolas, 'Courier New', Courier, monospace;
+  font-family: 'Fira Code', Inconsolata, Monaco, Consolas, 'Courier New',
+    Courier, monospace;
   direction: ltr;
   text-align: left;
   white-space: pre;

commit 67209895f188c6f261bc1b24d255f66ce91479b8

MDX: Show OG Image only when supplied through frontMatter
Caused by previous commit (44294055d1986ea811cd950ea812c5386ee433ca)

layouts/index.tsx
@@ -30,8 +30,12 @@ export default function MDXPage({ children, frontMatter }: Props) {
       </Head>
       <main>
         <h1 className={styles.blogHeading}>{frontMatter.title}</h1>
-	<img src={frontMatter.ogImage} width="100%" alt="" />
-	<h3>{frontMatter.description}</h3>
+        {frontMatter.ogImage ? (
+          <img src={frontMatter.ogImage} width="100%" alt="" />
+        ) : (
+          ''
+        )}
+        <h3>{frontMatter.description}</h3>
         {children}
       </main>
     </>