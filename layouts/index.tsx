8d84e4d6152b009a5150fef57f001f75a76808b9
thunder-coding <yakshbari4@gmail.com>
2021-05-28 15:37:25 +0530

    Validate frontMatter data

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -10,12 +10,50 @@ interface FrontMatter {
   tags: ReadonlyArray<string>;
 }
 
+/**
+ * This function will return an error if any problems are found inside front matter data.
+ * In case of any wrong value or something that seems to be very wrong,
+ * it will throw an error at build time so that this blogger won't mistakenly do some wrong thing
+ */
+// @prettier-ignore
+function validateFrontmatter({
+  title,
+  ogImage,
+  description,
+  publishedTime,
+  modifiedTime,
+  tags,
+}: FrontMatter) {
+  if (typeof title != 'string')
+    throw new Error('Blog title should be of type string');
+  if (title.length <= 20)
+    throw new Error('Title too short, consider having atleast 20 characters');
+  if (typeof description != 'string')
+    throw new Error('blog description should be of type string');
+  if (description.length <= 30)
+    throw new Error(
+      'Description too short, consider having atleast 30 characters'
+    );
+  if (tags.length > 8)
+    throw new Error(
+      'Spamming keywords may cause the site not to be indexed by search engines'
+    );
+  if (typeof modifiedTime != 'undefined') {
+    if (
+      new Date(modifiedTime).valueOf() - new Date(publishedTime).valueOf() <=
+      0
+    )
+      throw new Error('Modified time cannot be before published time');
+  }
+}
+
 interface Props {
   children: JSX.Element | JSX.Element[];
   frontMatter: FrontMatter;
 }
 
 export default function MDXPage({ children, frontMatter }: Props) {
+  validateFrontmatter(frontMatter);
   return (
     <>
       <Head>
