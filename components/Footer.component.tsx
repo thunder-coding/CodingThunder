8ab7c92e100b83abeda4f7fc36186f7f2f2272cb
thunder-coding <yakshbari4@gmail.com>
2021-05-30 15:49:46 +0530

    Revert to anchor tag for social links

--- components/Footer.component.tsx
+++ components/Footer.component.tsx
@@ -1,37 +1,35 @@
 import styles from './Footer.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Footer() {
   let year = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <div className={styles.socialLinks}>
-        <Link href="/codepen">
+        <a href="/codepen">
           <div className={styles.imgContainer}>
             <img src="/codepen.svg" alt="CodePen Logo" />
           </div>
-        </Link>
-        <Link href="/github">
+        </a>
+        <a href="/github">
           <div className={styles.imgContainer}>
             <img src="/github.svg" alt="GitHub Logo" />
           </div>
-        </Link>
-        <Link href="/twitter">
+        </a>
+        <a href="/twitter">
           <div className={styles.imgContainer}>
             <img src="/twitter.svg" alt="Twitter Logo" />
           </div>
-        </Link>
-        <Link href="/discord">
+        </a>
+        <a href="/discord">
           <div className={styles.imgContainer}>
             <img src="/discord.svg" alt="Discord Logo" />
           </div>
-        </Link>
-        <Link href="/linkedin">
+        </a>
+        <a href="/linkedin">
           <div className={styles.imgContainer}>
             <img src="/linkedin.svg" alt="LinkedIn Logo" />
           </div>
-        </Link>
+        </a>
       </div>
       <p className={styles.p}>© {year} Yaksh Bariya</p>
     </footer>
