200fbf48e10ea94e972c7abd1f64ab1bfe345747
Yaksh Bariya <yakshbari4@gmail.com>
2024-03-03 21:15:08 +0530

    Update website content

--- publish.sh
+++ publish.sh
@@ -3,10 +3,11 @@ set -e
 git add .
 git commit -m "Update website content"
 git push
+rm -rf public
 rm -rf ~/Projects/personal/thunder-coding.github.io/*
 hugo
 cp public/* ~/Projects/personal/thunder-coding.github.io/ -r
 cd ~/Projects/personal/thunder-coding.github.io/
 git add .
-git commit -m "Update website content"
-git push
+#git commit -m "Update website content"
+#git push
